c0bb3dd6f399dc9e39e74dcae14cf863a5dc5783
Fefe-Hern <fefehernandez@aim.com>
2015-06-29 00:37:39 -0400

    Added createLoginArea for testing.

--- src/atm/Tester.java
+++ src/atm/Tester.java
@@ -2,7 +2,8 @@ package atm;
 public class Tester {
     public static void main(String[] args) {
         //Read a file
-        CredentialsList.start();
+        CredentialsList.loadFile();
         CredentialsList.read("1121");
+        Login.createLoginArea();
     }
 }
